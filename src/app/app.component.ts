ca152b7ea619c8c48b72fd8f7944c298322a91da
ashu40h <157596646+ashu40h@users.noreply.github.com>
2025-08-01 21:31:30 +0530

    Head and home component basic code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from './head/head.component';
+import { HomeComponent } from './home/home.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeadComponent, HomeComponent],
+  styleUrls: ['./app.component.css'],
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
   title = 'SD360';
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { HeadComponent } from '../head/head.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; 
+import { Router } from '@angular/router';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeadComponent, NgFor, HttpClientModule], 
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  newsPosts: any[] = [];
+  screenWidth: number = 0;
+
+  constructor(private router: Router, private http: HttpClient) {}
+  ngOnInit() {
+    const url = 'https://raw.githubusercontent.com/ashu40h/files/main/news1.json';
+    if (typeof window !== 'undefined') {
+      this.screenWidth = window.innerWidth;
+    }
+    
+    console.log('Screen width:', this.screenWidth);
+
+    console.log('Initial screen width:', this.screenWidth);
+    this.http.get<any[]>(url).subscribe(
+      data => this.newsPosts = data,
+      error => console.error('Error loading posts:', error)
+
+    );
+  }
+  get newsDuration() {
+    return `${this.newsPosts.length * 4}s`; // auto sets scroll speed
+  }
+}
